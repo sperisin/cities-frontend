4171651838e6d1de26f97ba5cf1700b4f78b2351
Stjepan Perišin <stjepan.perisin@gmail.com>
2024-05-06 22:47:42 +0200

    refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tis-cities-angular'`, () => {
+  it(`should have as title 'cities-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tis-cities-angular');
+    expect(app.title).toEqual('cities-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tis-cities-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cities-frontend app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tis-cities-angular';
+  title = 'cities-frontend';
 }
